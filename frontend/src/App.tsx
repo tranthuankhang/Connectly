44e87dcb1c6153852c2643eb5bf6a254bb04a6f9
Fleeko <tranthuankhang05@gmail.com>
2026-05-20 19:50:06 +0700

    Update frontend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,35 @@
+import {BrowserRouter, Route, Routes} from 'react-router';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import ChatAppPage from './pages/ChatAppPage';
+import {Toaster} from 'sonner';
+
 function App() {
-  return <h1>Connectly Frontend is running</h1>;
+  return (<>
+    <Toaster richColors />
+    <BrowserRouter>
+      <Routes>
+
+        {/* public routes */}
+        <Route
+          path='/signin'
+          element={<SignInPage/>}
+        />
+        <Route
+          path='/signup'
+          element={<SignUpPage/>}
+        /> 
+  
+        {/* protected routes */}
+        {/* todo: tạo protected route */}
+        <Route
+          path='/'
+          element={<ChatAppPage/>}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  </>)
 }
 
 export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
