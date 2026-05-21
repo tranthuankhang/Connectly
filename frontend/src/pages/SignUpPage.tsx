07b27737aad78f5b387c5f901a68aeab96260436
Fleeko <tranthuankhang05@gmail.com>
2026-05-21 15:54:07 +0700

    Update sign up page

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { SignupForm } from "@/components/signup-form"
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <div className="relative flex min-h-svh flex-col items-center justify-center bg-orange-soft-glow p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <SignupForm />
+      </div>
+    </div>
   )
 }
 
